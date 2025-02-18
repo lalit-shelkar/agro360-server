b7ac5fb9fdab8868aa91aab0cae8fb898fa1ea78
lalit-shelkar <lalitshelkar2424@gmail.com>
2025-02-18 16:10:34 +0530

    added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const testRoutes = require("./routes/test"); // Import the test routes
-const serverless = require("serverless-http"); // For serverless deployment on Vercel
+//const serverless = require("serverless-http"); // For serverless deployment on Vercel
 const app = express();
 
 app.use(express.json()); // Middleware to parse JSON requests
@@ -9,4 +9,7 @@ app.use(express.json()); // Middleware to parse JSON requests
 app.use("/test", testRoutes);  // Routes will be prefixed with /test
 
 // Export the app as a serverless function
-module.exports.handler = serverless(app);
+//module.exports.handler = serverless(app);
+app.listen(3000, () => {
+    console.log("App is running")
+})
