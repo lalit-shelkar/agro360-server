97f7bd73ae5ec8001eebf8d1fd41622c0d35f298
lalit-shelkar <lalitshelkar2424@gmail.com>
2025-02-20 01:06:45 +0530

    added login

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require("express");
-const signup = require("./routes/signup"); // Import the test routes
+//const signup = require("./routes/signup"); // Import the test routes
 //const serverless = require("serverless-http"); // For serverless deployment on Vercel
 const app = express();
 const db = require("./config/database");
+const { signupController } = require("./controller/signupcontroller");
 db.connect();
+require("dotenv").config();
 
+const { PORT } = process.env;
 app.use(express.json()); // Middleware to parse JSON requests
 
 // Register test routes
-app.use("/api", signup);
+app.use("/signup", signupController);
 
+app.listen(PORT, () => {
+    console.log("App is running")
+})
 
 module.exports = app; 
