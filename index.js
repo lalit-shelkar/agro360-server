211b0e9b4665ea11692854a62f47cff9ca01cd5e
lalit-shelkar <lalitshelkar2424@gmail.com>
2025-02-20 15:33:49 +0530

    d

--- index.js
+++ index.js
@@ -6,16 +6,14 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const db = require("./config/database");
-const { signupcontroller } = require("./controller/signupcontroller");
+//const { signupcontroller } = require("./controller/signupcontroller");
 
 db.connect();
 
 
 //const PORT = 2000;
 // Middleware to parse JSON requests
-app.post("/signup", (req, res) => {
-    res.send("Welcome to Agro 360 v3");
-});
+app.post("/signup", signupcontroller);
 app.get("/", (req, res) => {
     res.send("Welcome to Agro 360 v3");
 });
@@ -27,3 +25,39 @@ app.listen(process.env.PORT, () => {
     console.log("App is running on port")
 })
 
+const signupcontroller = async (req, res) => {
+    try {
+        console.log("Signup request received:", req.body);
+
+        const { mobNumber } = req.body;
+
+        // Check if user already exists
+        const existingUser = await USER.findOne({ mobNumber });
+        if (existingUser) {
+            return res.status(400).json({ message: 'User already exists try login with pin' });
+        }
+
+        res.status(200).json({
+            message: 'Processing OTP request. Please check your phone soon.',
+            mobNumber,
+        });
+        // Generate OTP and expiration time
+        const { otp, expiresAt } = generateOtp();
+
+        // Store OTP temporarily (Use Redis in production)
+        otpStorage[mobNumber] = { otp, expiresAt };
+
+
+        // Send OTP
+        await sendOtp(mobNumber, otp);
+
+        // Return success response
+        return res.status(200).json({
+            message: 'OTP sent successfully. Please check your phone.',
+            mobNumber,  // Return the mobile number (to verify OTP later)
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Something went wrong. Please try again later.' });
+    }
+};
